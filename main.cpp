4ae93e217e4771011faf150412fe342ff44f06de
Amit Shaw <imalgorithmgeek@gmail.com>
2020-09-15 20:16:46 +0530

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[]) {
+
+    /* fast input-output */
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    /* get the number */
+    size_t number;
+    std::cin >> number;
+
+    /* print the number */
+    std::cout << number;
+
+    return 0;
+}
